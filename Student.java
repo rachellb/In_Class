5869e9efb96a27b98ba19ee6ce324a67b36c0916
Rachel Bennett <rachel.l.bennett-1@ou.edu>
2019-10-14 14:03:38 -0500

    Sketched a new class called Student (with rating)

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student implements Comparable<Student>{
+	private String className;
+	private Integer rating;
+	
+	@Override
+	public int compareTo(Student arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
